b59c63353abc5eafe7fa72808bc5bf39c61918c1
Levi <weil_fighting@163.com>
2015-06-18 12:25:06 +0800

    first version

--- src/com/stone/common/GlobalValues.java
+++ src/com/stone/common/GlobalValues.java
@@ -5,6 +5,9 @@ package com.stone.common;
  */
 public final class GlobalValues {
 	public static String OcrApiUrl = "http://apis.baidu.com/apistore/idlocr/ocr";
+	
+	//后台api接口地址
+	public static String ServiceApiUrl = "";
 
 	// SD卡中的根目录名
 	public static String ExtraFolderName = "DicCN";
